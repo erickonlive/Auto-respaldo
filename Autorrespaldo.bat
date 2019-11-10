6aa0cac94755aa0727d1b4a8a15e5d46e0b17eb2
erickonlive <erick13@quetevalga.com>
2019-11-10 12:16:03 -0700

    Update Autorrespaldo.bat

--- Autorrespaldo.bat
+++ Autorrespaldo.bat
@@ -7,29 +7,18 @@ fn.dll sprite 19 25 13 "MDM CONTINENTAL"
 :CHECK
 fn.dll sleep 500
 IF NOT EXIST d:\ fn.dll sprite 03 08 0A "Inserte una memoria USB"&goto check
-IF %date:~3,2%==01 (set mes=ENERO& GOTO DOWN) else (goto 1)
-:1
-IF %date:~3,2%==02 (set mes=FEBRERO& GOTO DOWN) else (goto 2)
-:2
-IF %date:~3,2%==03 (set mes=MARZO& GOTO DOWN) else (goto 3)
-:3
-IF %date:~3,2%==04 (set mes=ABRIL& GOTO DOWN) else (goto 4)
-:4
-IF %date:~3,2%==05 (set mes=MAYO& GOTO DOWN) else (goto 5)
-:5
-IF %date:~3,2%==06 (set mes=JUNIO& GOTO DOWN) else (goto 6)
-:6
-IF %date:~3,2%==07 (set mes=JULIO& GOTO DOWN) else (goto 7)
-:7
-IF %date:~3,2%==08 (set mes=AGOSTO& GOTO DOWN) else (goto 8)
-:8
-IF %date:~3,2%==09 (set mes=SEPTIEMBRE& GOTO DOWN) else (goto 9)
-:9
-IF %date:~3,2%==10 (set mes=OCTUBRE& GOTO DOWN) else (goto 10)
-:10
-IF %date:~3,2%==11 (set mes=NOVIEMBRE& GOTO DOWN) else (goto 11)
-:11
-IF %date:~3,2%==12 (set mes=DICIEMBRE& GOTO DOWN) else (goto Check)
+IF %date:~3,2%==01 set mes=ENERO
+IF %date:~3,2%==02 set mes=FEBRERO
+IF %date:~3,2%==03 set mes=MARZO
+IF %date:~3,2%==04 set mes=ABRIL
+IF %date:~3,2%==05 set mes=MAYO
+IF %date:~3,2%==06 set mes=JUNIO
+IF %date:~3,2%==07 set mes=JULIO
+IF %date:~3,2%==08 set mes=AGOSTO
+IF %date:~3,2%==09 set mes=SEPTIEMBRE
+IF %date:~3,2%==10 set mes=OCTUBRE
+IF %date:~3,2%==11 set mes=NOVIEMBRE
+IF %date:~3,2%==12 set mes=DICIEMBRE
 :down
 cls
 fn.dll sprite 19 25 13 "MDM CONTINENTAL"
@@ -51,6 +40,6 @@ fn.dll sleep 3000
 cls
 fn.dll sprite 09 02 0a "Subiendo archivo del %date:~0,2% de %mes%"
 fn.dll sprite 10 02 0a "Esto puede tardar unos minutos"
-D:\gdrive upload -r "D:\RESPALDOS\%DATE:~6,4%\%MES%\%date:~0,2% DE %mes%\%date:~0,2% DE %mes%.rar" -p 1ZOOOvVJBQwhI7hpPMxGqwgmJSSevu4dQ
+D:\gdrive upload -r "D:\RESPALDOS\%DATE:~6,4%\%MES%\%date:~0,2% DE %mes%\%date:~0,2% DE %mes%.rar" -p "Google folder code"
 fn.dll sprite 09 01 0C "El archivo a sido subido"
 fn.dll sleep 3000
